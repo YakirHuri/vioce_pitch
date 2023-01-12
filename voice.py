9e78cccdf450c4fd65de80622798f1d1be64d13f
yakir <yakir@cogniteam.com>
2023-01-12 10:57:52 +0200

    wip

--- voice.py
+++ voice.py
@@ -14,9 +14,14 @@ import queue
 import soundfile as sf
 import threading
 from tkinter import messagebox
+from scipy.io import wavfile
+from scipy.fftpack import fft
 
+import numpy as np
 from PIL import Image, ImageTk
 import pyrubberband as pyrb
+
+import matplotlib.pyplot as plt
 
 from pydub import AudioSegment
 from pydub.effects import speedup
@@ -29,6 +34,56 @@ class Recorder:
      
         self.octaves = 0.0
         self.q = queue.Queue()    
+
+        # Define the user interface for Voice Recorder using Python
+        self.voice_rec = Tk()
+
+        self.voice_rec.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.voice_rec.geometry("500x200")
+        self.voice_rec.title("  YONI - תוכנת עיוות קול")
+        self.voice_rec.config(bg="white")
+        self.voice_rec.resizable(False, False)
+        # Create a queue to contain the audio data
+        q = queue.Queue()
+        # Declare variables and initialise them
+        recording = False
+        file_exists = False  
+
+        ##### OCTAVE ##########################
+        octave_label = Label(self.voice_rec, text="octave", bg="white").grid(
+            row=40, column=0, pady=4, padx=4)
+        octave_label_s = Scale(self.voice_rec, from_=-5, to=15,
+                            orient=HORIZONTAL, length=300, bg="white", command=self.setOctave)
+        octave_label_s.set(0)
+        octave_label_s.grid(row=40, column=1, pady=4, padx=4)
+
+        # Button to record audio
+        im_re = Image.open('record.jpg')
+        ph_re= ImageTk.PhotoImage(im_re)
+        record_btn = Button(self.voice_rec, text="RECORD", image=ph_re, 
+                            command=lambda m=1: self.threading_rec(m))
+        # Stop button
+        im_st = Image.open('stop.jpg')
+        ph_st= ImageTk.PhotoImage(im_st)
+        stop_btn = Button(self.voice_rec, text="STOP RECORDING", image=ph_st, 
+                        command=lambda m=2: self.threading_rec(m))
+        # Play button
+        im_pl = Image.open('play.jpg')
+        ph_pl= ImageTk.PhotoImage(im_pl)    
+        play_btn = Button(self.voice_rec, text="PLAY RECORDING", image=ph_pl, 
+                        command=lambda m=3: self.threading_rec(m))
+        # Position buttons
+        record_btn.grid(row=1, column=1)
+        stop_btn.grid(row=1, column=0)
+        play_btn.grid(row=1, column=2)
+        self.voice_rec.mainloop()
+
+    def close(self):
+        print('close')
+        self.voice_rec.destroy()
+        self.voice_rec.quit()
+        exit(1)
 
     def setOctave(self, octaves):
         self.octaves = float(octaves) / 10.0   
@@ -52,7 +107,6 @@ class Recorder:
         #Play pitch changed sound
         play(hipitch_sound)    
         
-        y_stretch = pyrb.time_stretch(hipitch_sound, sr, 0.75)
 
   
     # Functions to play, stop and record audio in Python voice recorder
@@ -75,6 +129,11 @@ class Recorder:
                 # Read the recording if it exists and play it
 
                 print('play the recording')
+
+                sample_rate, audio_time_series = wavfile.read('voice.wav')
+
+                self.fft_plot(audio_time_series, sample_rate)
+
                 self.change_octave('voice.wav')
 
             else:
@@ -105,51 +164,25 @@ class Recorder:
                     # write into file
                     file.write(self.q.get())
 
+    def fft_plot(self, audio, sample_rate):
+        N = len(audio)    # Number of samples
+        y_freq = fft(audio)
+        domain = len(y_freq) // 2
+        x_freq = np.linspace(0, sample_rate//2, N//2)
+        # Changed from abs(y_freq[:domain]) -> y_freq[:domain]
+        plt.plot(x_freq, y_freq[:domain])
+        plt.xlabel("Frequency [Hz]")
+        plt.ylabel("Frequency Amplitude |X(t)|")
+
+        plt.savefig('plt.png')
+
 
   
 def main():
+   
 
     recorder = Recorder()
-    # Define the user interface for Voice Recorder using Python
-    voice_rec = Tk()
-    voice_rec.geometry("500x200")
-    voice_rec.title("  YONI - תוכנת עיוות קול")
-    voice_rec.config(bg="white")
-    voice_rec.resizable(False, False)
-    # Create a queue to contain the audio data
-    q = queue.Queue()
-    # Declare variables and initialise them
-    recording = False
-    file_exists = False  
-
-    ##### OCTAVE ##########################
-    octave_label = Label(voice_rec, text="octave", bg="white").grid(
-        row=40, column=0, pady=4, padx=4)
-    octave_label_s = Scale(voice_rec, from_=-5, to=15,
-                         orient=HORIZONTAL, length=300, bg="white", command=recorder.setOctave)
-    octave_label_s.set(0)
-    octave_label_s.grid(row=40, column=1, pady=4, padx=4)
-
-    # Button to record audio
-    im_re = Image.open('record.jpg')
-    ph_re= ImageTk.PhotoImage(im_re)
-    record_btn = Button(voice_rec, text="RECORD", image=ph_re, 
-                        command=lambda m=1: recorder.threading_rec(m))
-    # Stop button
-    im_st = Image.open('stop.jpg')
-    ph_st= ImageTk.PhotoImage(im_st)
-    stop_btn = Button(voice_rec, text="STOP RECORDING", image=ph_st, 
-                      command=lambda m=2: recorder.threading_rec(m))
-    # Play button
-    im_pl = Image.open('play.jpg')
-    ph_pl= ImageTk.PhotoImage(im_pl)    
-    play_btn = Button(voice_rec, text="PLAY RECORDING", image=ph_pl, 
-                      command=lambda m=3: recorder.threading_rec(m))
-    # Position buttons
-    record_btn.grid(row=1, column=1)
-    stop_btn.grid(row=1, column=0)
-    play_btn.grid(row=1, column=2)
-    voice_rec.mainloop()
+    
 
 
 if __name__ == '__main__':
